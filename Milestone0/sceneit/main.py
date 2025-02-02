d013b88327474af14b8c03fd0bf3f17a851c7adc
Xiang Li <xiangli3625@gmail.com>
2025-02-02 12:49:28 -0500

    look at movie table

--- Milestone0/sceneit/main.py
+++ Milestone0/sceneit/main.py
@@ -24,3 +24,17 @@ setup_database()
 @app.get("/")
 def read_root():
     return {"message": "hello world sceneit"}
+
+@app.get("/movie_data")
+def get_values():
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM Movie;")
+                rows = cur.fetchall()
+                columns = [desc[0] for desc in cur.description]  # Get column names
+
+        return [dict(zip(columns, row)) for row in rows]  # Convert to list of dictionaries
+
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching values: {e}")
